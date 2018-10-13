137af5c1840c2e93c1c85217bae051eba70eecdf
Oscar Pachano <opachano@gmail.com>
2018-10-12 20:09:27 -0400

    It's easier to locate individual rovers now

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,7 @@
 //Grid Goes Here
 
 let grid = [
-  ["rover", "stone", "stone", "clear", "stone", "clear", "clear", "clear", "clear", "rover"],
+  ["rover1", "stone", "stone", "clear", "stone", "clear", "clear", "clear", "clear", "rover4"],
   ["clear", "clear", "clear", "clear", "stone", "stone", "clear", "clear", "clear", "clear"],
   ["clear", "clear", "clear", "clear", "clear", "clear", "clear", "clear", "stone", "clear"],
   ["clear", "clear", "stone", "clear", "clear", "clear", "clear", "clear", "stone", "clear"],
@@ -10,34 +10,38 @@ let grid = [
   ["clear", "clear", "clear", "clear", "clear", "clear", "clear", "clear", "clear", "clear"],
   ["stone", "clear", "clear", "clear", "stone", "clear", "clear", "clear", "clear", "clear"],
   ["stone", "clear", "clear", "clear", "clear", "clear", "clear", "clear", "stone", "stone"],
-  ["rover", "clear", "clear", "clear", "clear", "clear", "clear", "clear", "clear", "rover"]
+  ["rover3", "clear", "clear", "clear", "clear", "clear", "clear", "clear", "clear", "rover2"]
 ];
 
 // Rover Object Goes Here
 
 let rover1 = {
-  direction: "N",
+  name: "rover1",
+  direction: "E",
   x: 0,
   y: 0,
   travelLog: [[0,0]],
 };
 
 let rover2 = {
-  direction: "S",
+  name: "rover2",
+  direction: "W",
   x: 9,
   y: 9,
   travelLog: [[9,9]],
 };
 
 let rover3 = {
-  direction: "W",
+  name: "rover3",
+  direction: "N",
   x: 0,
   y: 9,
   travelLog: [[0,9]],
 };
 
 let rover4 = {
-  direction: "E",
+  name: "rover4",
+  direction: "S",
   x: 9,
   y: 0,
   travelLog: [[9,0]],
@@ -78,30 +82,27 @@ function moveForward(rover) {
     };
     grid[rover.y][rover.x] = "clear";
     rover.y--;
-    grid[rover.y][rover.x] = "rover";
   } else if (rover.direction === "S" && rover.y !== 9) {
     if (grid[rover.y + 1][rover.x] !== "clear") {
       return console.log("ERROR: " + grid[rover.y + 1][rover.x] + " found, can't proceed!");
     };
     grid[rover.y][rover.x] = "clear";
     rover.y++;
-    grid[rover.y][rover.x] = "rover";
   } else if (rover.direction === "E" && rover.x !== 9) {
     if (grid[rover.y][rover.x + 1] !== "clear") {
       return console.log("ERROR: " + grid[rover.y][rover.x + 1] + " found, can't proceed!");
     };
     grid[rover.y][rover.x] = "clear";
     rover.x++;
-    grid[rover.y][rover.x] = "rover";
   } else if (rover.direction === "W" && rover.x !== 0) {
     if (grid[rover.y][rover.x - 1] !== "clear") {
       return console.log("ERROR: " + grid[rover.y][rover.x - 1] + " found, can't proceed!");
     };
     grid[rover.y][rover.x] = "clear";
     rover.x--;
-    grid[rover.y][rover.x] = "rover";
   }   else return console.log("ERROR: ROVER CAN'T MOVE OUT OFF GRID!");
   rover.travelLog.push([rover.x, rover.y]);
+  grid[rover.y][rover.x] = rover.name;
   console.log("Rover located at: " + rover.x + "," + rover.y);
 };
 
@@ -111,31 +112,28 @@ function moveBackward(rover){
       return console.log("ERROR: " + grid[rover.y + 1][rover.x] + " found, can't proceed!");
     };
     grid[rover.y][rover.x] = "clear";
-    rover.y++;
-    grid[rover.y][rover.x] = "rover";
+    rover.y++;   
   } else if (rover.direction === "S" && rover.y !== 0) {
     if (grid[rover.y - 1][rover.x] !== "clear") {
       return console.log("ERROR: " + grid[rover.y - 1][rover.x] + " found, can't proceed!");
     };
     grid[rover.y][rover.x] = "clear";
     rover.y--;
-    grid[rover.y][rover.x] = "rover";
   } else if (rover.direction === "E" && rover.x !== 0) {
     if (grid[rover.y][rover.x - 1] !== "clear") {
       return console.log("ERROR: " + grid[rover.y][rover.x - 1] + " found, can't proceed!");
     };
     grid[rover.y][rover.x] = "clear";
     rover.x--;
-    grid[rover.y][rover.x] = "rover";
   } else if (rover.direction === "W" && rover.x !== 9) {
     if (grid[rover.y][rover.x + 1] !== "clear") {
       return console.log("ERROR: " + grid[rover.y][rover.x + 1] + " found, can't proceed!");
     };
     grid[rover.y][rover.x] = "clear";
     rover.x++;
-    grid[rover.y][rover.x] = "rover";
   }   else return console.log("ERROR: ROVER CAN'T MOVE OUT OFF GRID!");
   rover.travelLog.push([rover.x, rover.y]);
+  grid[rover.y][rover.x] = rover.name;
   console.log("Rover located at: " + rover.x + "," + rover.y);
 };
 
